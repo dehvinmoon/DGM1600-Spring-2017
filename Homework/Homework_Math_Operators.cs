9b4b47dd3c85c410e0b47423f75987e06a07a319
Dehvan Moon <dehvinmoon@gmail.com>
2017-02-09 19:12:14 -0700

    Homework and unity projects.

---
+++ Homework/Homework_Math_Operators.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Operators : MonoBehaviour {
+	public int one =1;
+	public int two =2;
+
+//Use for Intalizing
+void Start() {
+
+}
+//Update is called once per frame
+void Update () {
+
+//Addition: Adds and leaves the sum.
+Debug. Log(one + two);
+
+//Modulus: Leaves the remainder of the two varriables.
+Debug.Log(one % two);
+
+//Subtraction: Subtracts and leaves the sum.
+Debug.Log(one - two);
+
+//Division: Divides and leaves the sum.
+Debug.Log(one / two);
+
+//Multiplication: Multiplies and leaves the sum.
+Debug.Log(one * two);
+
+//Increment: Each + sign is an increment one added upon the number chosen.
+Debug.Log(one + + two);
+
+//Decrement: Each - sign is an increment one substracted upon the number chosen.
+Debug.Log(one - - two);
+
+
+
+}
+
